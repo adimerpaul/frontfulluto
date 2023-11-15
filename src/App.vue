bb84720a0fdbe0f73c2fa330f9c44e8518e89ae3
adimerpaul <adimer101@gmail.com>
2023-11-14 20:18:29 -0400

    proto de sumar numero

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h3>Suma de 2 numeros</h3>
+    <input
+    v-model="num1"
+    type="number"/>
+    <input
+    v-model="num2"
+    type="number"/>
+    <br>
+    <button @click="sumar">sumar</button>
+    <br>
+    <p>resultado {{ res }}</p>
+    <Hola></Hola>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Hola from "@/components/Hola.vue"
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Hola
+  },
+  data(){
+    return{
+      num1: 45,
+      num2: 30,
+      res: 0
+    }
+  },
+  methods :{
+    sumar(){
+      this.res= this.num1+this.num2
+    }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html{
+  background: gray;
 }
 </style>
---
+++ src/components/Hola.vue
@@ -0,0 +1,8 @@
+<template>
+    Hola, lo que quiera
+</template>
+<script>
+export default {
+  name: 'HolaComponentes',
+}
+</script>
